558e174d741ffe7a4e8560e94a5119bcac0e107d
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-07-30 16:03:28 -0300

    Add project files.

---
+++ WebAPIVersion/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIVersion.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAPIVersion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIVersion.WebApiApplication" Language="C#" %>
